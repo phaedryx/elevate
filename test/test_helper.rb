a1ff81d7c94c646a84c468088bd169c201cefe5f
Tad Thorley <phaedryx@gmail.com>
2020-05-17 17:18:10 -0600

    yay! a passing test

---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+require 'bundler'
+Bundler.require(:test)
+require 'minitest/autorun'
+require 'minitest/focus'
+require 'minitest/spec'
---
+++ test/truth_test.rb
@@ -0,0 +1,7 @@
+require_relative 'test_helper'
+
+describe 'truth' do
+  it 'should assert truth' do
+    assert true
+  end
+end
